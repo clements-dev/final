1e0ec5fef633ca7639afe753c5d9b19cb3cc1750
clements-dev <59267018+clements-dev@users.noreply.github.com>
2020-03-15 01:24:57 -0500

    update

--- app.rb
+++ app.rb
@@ -14,5 +14,150 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
+
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    @events_request = events_table.where(event_type: "request").all.to_a
+    @events_coaching = events_table.where(event_type: "coaching").all.to_a
+    view "events"
+end
+
+get "/events/new-1" do
+    view "new_event1"
+end
+
+get "/events/new-2" do
+    # Find the location coordinates
+    results = Geocoder.search(params["set_location"])
+
+    # Get location coordinates
+    location_coord = results.first.coordinates # => [lat, long]
+    @country_code = results.first.country_code
+    city = results.first.city
+    country = results.first.country
+    if city==""
+        @location_txt = country
+    else
+        @location_txt = country + "&emsp;|&emsp;" + city
+    end
+    @location_lat = location_coord[0]
+    @location_long = location_coord[1]
+
+    # Open second step page
+    view "new_event2"
+end
+
+get "/events/create" do
+    @event = events_table.where(id: params["id"]).to_a[0]
+    @userp_name = users_table.where(id: session["user_id"]).to_a[1]
+    
+    if params["event_type"]=="DIY support request"
+        event_t = "request"
+    else
+        event_t = "coaching"
+    end
+
+    events_table.insert(user_id: session["user_id"],
+                        event_type: event_t,
+                        title: params["title"],
+                        description: params["description"],
+                        date:  params["month"] + " " + params["day"] + ", " + params["year"],
+                        location: params["location"],
+                        lat: params["lat"],
+                        lon: params["lon"])
+
+    view "create_event"
+end
+
+get "/events/:id" do
+    @event = events_table.where(id: params[:id]).to_a[0]
+    @rsvps = rsvps_table.where(event_id: @event[:id])
+    @attending_count = rsvps_table.where(event_id: @event[:id], attending: true).count
+    @users_table = users_table
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(id: params[:id]).to_a[0]
+    view "new_rsvp"
+end
+
+get "/events/:id/rsvps/create" do
+    @event = events_table.where(id: params["id"]).to_a[0]
+    @userp_name = users_table.where(id: session["user_id"]).to_a[1]
+    rsvps_table.insert(event_id: params["id"],
+                       user_id: session["user_id"],
+                       name: @userp_name,
+                       attending: params["attending"],
+                       comments: params["comments"])
+    view "create_rsvp"
+end
+
+get "/users/new-1" do
+    # Open first step sign up
+    view "new_user1"
+end
+
+get "/users/new-2" do
+    # Find the location coordinates
+    results = Geocoder.search(params["set_location"])
+
+    # Get location coordinates
+    location_coord = results.first.coordinates # => [lat, long]
+    @country_code = results.first.country_code
+    city = results.first.city
+    country = results.first.country
+    if city==""
+        @location_txt = country
+    else
+        @location_txt = country + "&emsp;|&emsp;" + city
+    end
+    @location_lat = location_coord[0]
+    @location_long = location_coord[1]
+
+    #Open second step page
+    view "new_user2"
+end
+
+post "/users/create" do
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], 
+                        email: params["email"], 
+                        phone: params["phone"], 
+                        password: hashed_password, 
+                        location: params["location"], 
+                        lat: params["lat"], 
+                        lon: params["lon"])
+    @signup_user = params["name"]
+
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
--- createdb.rb
+++ createdb.rb
@@ -1,5 +1,6 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
 require "sequel"                                                                      #
+require "bcrypt"                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB = Sequel.connect(connection_string)                                                #
 #######################################################################################
@@ -7,29 +8,60 @@ DB = Sequel.connect(connection_string)                                          
 # Database schema - this should reflect your domain model
 DB.create_table! :events do
   primary_key :id
+  foreign_key :user_id
+  String :event_type
   String :title
   String :description, text: true
   String :date
   String :location
+  Double :lat
+  Double :lon
 end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
-  Boolean :going
+  foreign_key :user_id
+  Boolean :attending
+  String :name
+  String :comments, text: true
+end
+
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
+  String :location
+  Double :lat
+  Double :lon
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
+users_table = DB.from(:users)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "Final day - KIEI_451-0_SEC81", 
+                    user_id: 0,
+                    event_type: "coaching",
+                    description: "End of course!",
+                    date: "March 9, 2020",
+                    location: "Chicago",
+                    lat: 0,
+                    lon: 0)
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "Final Project - KIEI_451-0_SEC81", 
+                    user_id: 0,
+                    event_type: "request",
+                    description: "Last project!",
+                    date: "March 15, 2020",
+                    location: "Chicago",
+                    lat: 0,
+                    lon: 0)
+
+cdb_hashed_password = BCrypt::Password.create("admin")
+users_table.insert(name: "admin",
+                    email: "admin",
+                    password: cdb_hashed_password,
+                    location: "Chicago",
+                    lat: 0,
+                    lon: 0)
